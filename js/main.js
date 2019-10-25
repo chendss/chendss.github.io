b38ccdec6072dabfc52bfd782b65d4252e1da285
dashao <dashao@outlook.com>
2019-10-26 03:10:56 +0800

    Site updated: 2019-10-26 03:10:56

--- js/main.js
+++ js/main.js
@@ -1,129 +1,128 @@
-function scrollToElement (target, offset) {
-  var scroll_offset = $(target).offset();
-  $("body,html").animate({
-    scrollTop: scroll_offset.top + (offset || 0),
-    easing: 'swing'
-  })
+function scrollToElement(target, offset) {
+	var scroll_offset = $(target).offset()
+	$('body,html').animate({
+		scrollTop: scroll_offset.top + (offset || 0),
+		easing: 'swing',
+	})
 }
 
-function scrollToBoard () {
-  scrollToElement('#board', -$("#navbar").height());
+function scrollToBoard() {
+	scrollToElement('#board', -$('#navbar').height())
 }
 
-document.getElementById('board').onload = scrollToBoard;
+document.getElementById('board').onload = scrollToBoard
 
+$(document).ready(function() {
+	var navbar = $('#navbar')
+	if (navbar.offset().top > 0) {
+		navbar.addClass('navbar-custom')
+		navbar.removeClass('navbar-dark')
+	}
+	$(window).scroll(function() {
+		if (navbar.offset().top > 0) {
+			navbar.addClass('navbar-custom')
+			navbar.removeClass('navbar-dark')
+		} else {
+			navbar.addClass('navbar-dark')
+		}
+	})
+	$('#navbar-toggler-btn').on('click', function() {
+		$('.animated-icon').toggleClass('open')
+		$('#navbar').toggleClass('navbar-col-show')
+	})
 
-$(document).ready(function () {
-  var navbar = $("#navbar");
-  if (navbar.offset().top > 0) {
-    navbar.addClass("navbar-custom");
-    navbar.removeClass("navbar-dark");
-  }
-  $(window).scroll(function () {
-    if (navbar.offset().top > 0) {
-      navbar.addClass("navbar-custom");
-      navbar.removeClass("navbar-dark");
-    } else {
-      navbar.addClass("navbar-dark");
-    }
-  });
-  $('#navbar-toggler-btn').on('click', function () {
-    $('.animated-icon').toggleClass('open');
-    $('#navbar').toggleClass('navbar-col-show');
-  });
+	var oldLoad = window.onload
+	window.onload = function() {
+		oldLoad && oldLoad()
+	}
 
-  var oldLoad = window.onload;
-  window.onload = function () {
-    oldLoad && oldLoad();
-  };
+	// 向下滚动箭头的点击
+	$('.scroll-down-bar').on('click', scrollToBoard)
 
-  // 向下滚动箭头的点击
-  $(".scroll-down-bar").on("click", scrollToBoard);
+	// 向顶部滚动箭头
+	var topArrow = $('#scroll-top-button')
+	var posDisplay = false
+	var scrollDisplay = false
+	// 位置
+	var setTopArrowPos = function() {
+		var boardRight = document.getElementById('board').getClientRects()[0].right
+		var bodyWidth = document.body.offsetWidth
+		var right = bodyWidth - boardRight
+		posDisplay = right >= 50
+		topArrow.css({
+			bottom: posDisplay && scrollDisplay ? '20px' : '-60px',
+			right: right - 64 + 'px',
+		})
+	}
+	setTopArrowPos()
+	$(window).resize(setTopArrowPos)
+	// 显示
+	var headerHeight = $('#board').offset().top
+	$(window).scroll(function() {
+		var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop
+		scrollDisplay = scrollHeight >= headerHeight
+		topArrow.css({
+			bottom: posDisplay && scrollDisplay ? '20px' : '-60px',
+		})
+	})
+	// 点击
+	topArrow.on('click', function() {
+		$('body,html').animate({
+			scrollTop: 0,
+			easing: 'swing',
+		})
+	})
+})
 
-  // 向顶部滚动箭头
-  var topArrow = $("#scroll-top-button");
-  var posDisplay = false;
-  var scrollDisplay = false;
-  // 位置
-  var setTopArrowPos = function () {
-    var boardRight = document.getElementById('board').getClientRects()[0].right;
-    var bodyWidth = document.body.offsetWidth;
-    var right = bodyWidth - boardRight;
-    posDisplay = right >= 50;
-    topArrow.css({
-      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px",
-      "right": right - 64 + "px"
-    });
-  };
-  setTopArrowPos();
-  $(window).resize(setTopArrowPos);
-  // 显示
-  var headerHeight = $("#board").offset().top;
-  $(window).scroll(function () {
-    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
-    scrollDisplay = scrollHeight >= headerHeight;
-    topArrow.css({
-      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px"
-    });
-  });
-  // 点击
-  topArrow.on("click", function () {
-    $("body,html").animate({
-      scrollTop: 0,
-      easing: 'swing'
-    })
-  });
-});
-
-const getScrollTop = function () {
-  var scroll_top = 0;
-  if (document.documentElement && document.documentElement.scrollTop) {
-    scroll_top = document.documentElement.scrollTop
-  }
-  else if (document.body) {
-    scroll_top = document.body.scrollTop
-  }
-  return scroll_top
+const getScrollTop = function() {
+	var scroll_top = 0
+	if (document.documentElement && document.documentElement.scrollTop) {
+		scroll_top = document.documentElement.scrollTop
+	} else if (document.body) {
+		scroll_top = document.body.scrollTop
+	}
+	return scroll_top
 }
 
-const throttle = function (func, delay) {
-  var prev = Date.now()
-  return function () {
-    var context = this
-    var args = arguments
-    var now = Date.now()
-    if (now - prev >= delay) {
-      func.apply(context, args)
-      prev = Date.now()
-    }
-  }
+const throttle = function(func, delay) {
+	var prev = Date.now()
+	return function() {
+		var context = this
+		var args = arguments
+		var now = Date.now()
+		if (now - prev >= delay) {
+			func.apply(context, args)
+			prev = Date.now()
+		}
+	}
 }
 
 class Queue {
-  constructor() {
-    this.list = []
-    this.lock = false
-    setInterval(() => {
-      if (!this.lock) {
-        const handle = this.list.pop()
-        handle && handle()
-        this.list = []
-      }
-    }, 50)
-  }
-  add (handle) {
-    this.lock = true
-    this.list.push(handle)
-    this.lock = false
-  }
+	constructor() {
+		this.list = []
+		this.lock = false
+		setInterval(() => {
+			if (!this.lock) {
+				const handle = this.list.pop()
+				handle && handle()
+				this.list = []
+			}
+		}, 50)
+	}
+	add(handle) {
+		this.lock = true
+		this.list.push(handle)
+		this.lock = false
+	}
 }
 
 const queue = new Queue()
 
-const scrollHandle = function () {
-  const baseTop = document.querySelector('header').offsetHeight - 50
-  const dom = document.querySelector('#toc')
-  dom.setAttribute('style', `margin-top:${Math.max(0, getScrollTop() - baseTop)}px`)
+const scrollHandle = function() {
+	const baseTop = document.querySelector('header').offsetHeight - 50
+	const dom = document.querySelector('#toc')
+	const top = `${Math.max(0, getScrollTop() - baseTop)}px`
+	$(dom).css({ 'margin-top': top })
 }
 
-// window.addEventListener('scroll', throttle(scrollHandle, 25))
+window.addEventListener('scroll', throttle(scrollHandle, 25))
--- js/post.js
+++ js/post.js
@@ -1,35 +1,32 @@
-$(document).ready(function () {
-  var navHeight = $("#navbar").height();
-  var toc = $("#toc");
-  var main = $("main");
-  var tocT = navHeight + (toc.offset().top - main.offset().top);
-  var tocLimMin = main.offset().top - navHeight;
-  var tocLimMax = $("#comments").offset().top - navHeight;
-  $(window).scroll(function () {
-    const navH = document.querySelector('#navbar').offsetHeight
-    var scroH = document.body.scrollTop + document.documentElement.scrollTop;
-    if (document.querySelector('.container-fluid').getBoundingClientRect().y <= navH) {
-      toc.css({
-        "display": "block",
-        "position": "fixed",
-        "top": navH
-      })
-    } else {
-      toc.css({
-        "position": "",
-        "top": ''
-      })
-    }
-  });
-  tocbot.init({
-    tocSelector: '#tocbot',
-    contentSelector: '.post-content',
-    headingSelector: 'h1, h2, h3, h4, h5, h6',
-    linkClass: 'tocbot-link',
-    activeLinkClass: 'tocbot-active-link',
-    listClass: 'tocbot-list',
-    isCollapsedClass: 'tocbot-is-collapsed',
-    collapsibleClass: 'tocbot-is-collapsible',
-    scrollSmooth: true,
-  });
-});
+$(document).ready(function() {
+	var navHeight = $('#navbar').height()
+	var toc = $('#toc')
+	var main = $('main')
+	var tocT = navHeight + (toc.offset().top - main.offset().top)
+	var tocLimMin = main.offset().top - navHeight
+	var tocLimMax = $('#comments').offset().top - navHeight
+	$(window).scroll(function() {
+		const navH = document.querySelector('#navbar').offsetHeight
+		var scroH = document.body.scrollTop + document.documentElement.scrollTop
+		if (document.querySelector('.container-fluid').getBoundingClientRect().y <= navH) {
+			toc.css({
+				display: 'block',
+			})
+		} else {
+			toc.css({
+				position: '',
+			})
+		}
+	})
+	tocbot.init({
+		tocSelector: '#tocbot',
+		contentSelector: '.post-content',
+		headingSelector: 'h1, h2, h3, h4, h5, h6',
+		linkClass: 'tocbot-link',
+		activeLinkClass: 'tocbot-active-link',
+		listClass: 'tocbot-list',
+		isCollapsedClass: 'tocbot-is-collapsed',
+		collapsibleClass: 'tocbot-is-collapsible',
+		scrollSmooth: true,
+	})
+})
